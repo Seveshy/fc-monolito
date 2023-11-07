4b720aea82da80056c3762fcb4d25583e80da6d5
Daniel Major <daniel.alves@skytef.com.br>
2023-11-07 20:47:16 -0300

    Criando interface da nossa Facade

---
+++ src/modules/@shared/domain/product-adm/repository/product.repository.spec.ts
@@ -0,0 +1,48 @@
+import { Sequelize } from "sequelize-typescript";
+import Product from "../domain/product.entity";
+import ProductRepository from "./product.repository";
+import Id from "../../value-object/id.value-object";
+import { ProductModel } from "./product.model";
+
+describe("ProductRepository test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should create a product", async () => {
+    const productProps = {
+      id: new Id("1"),
+      name: "Product 1",
+      description: "Product 1 description",
+      purchasePrice: 100,
+      stock: 10,
+    };
+    const product = new Product(productProps);
+    const productRepository = new ProductRepository();
+    await productRepository.add(product);
+
+    const productDb = await ProductModel.findOne({
+      where: { id: productProps.id.id },
+    });
+
+    expect(productProps.id.id).toEqual("1");
+    expect(productProps.name).toEqual("Product 1");
+    expect(productProps.description).toEqual("Product 1 description");
+    expect(productProps.purchasePrice).toEqual(100);
+    expect(productProps.stock).toEqual(10);
+  });
+});
