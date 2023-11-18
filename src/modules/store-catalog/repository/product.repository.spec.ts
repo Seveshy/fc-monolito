d236f32ebd388aa2d25ed4d087a85783e494d23b
Daniel Major <daniel.alves@skytef.com.br>
2023-11-18 11:55:22 -0300

    Implementando FindProductUseCase

--- src/modules/store-catalog/repository/product.repository.spec.ts
+++ src/modules/store-catalog/repository/product.repository.spec.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import ProductModel from "./product.model";
+import ProductRepository from "./product.repository";
 
 describe("ProductRepository test", () => {
   let sequelize: Sequelize;
@@ -36,14 +37,14 @@ describe("ProductRepository test", () => {
     });
 
     const productRepository = new ProductRepository();
-    const products = await productRepository.find()
+    const products = await productRepository.findAll();
 
     expect(products.length).toBe(2);
-    expect(products[0].id).toBe("1");
+    expect(products[0].id.id).toBe("1");
     expect(products[0].name).toBe("Product 1");
     expect(products[0].description).toBe("Description 1");
     expect(products[0].salesPrice).toBe(100);
-    expect(products[1].id).toBe("2");
+    expect(products[1].id.id).toBe("2");
     expect(products[1].name).toBe("Product 2");
     expect(products[1].description).toBe("Description 2");
     expect(products[1].salesPrice).toBe(200);
