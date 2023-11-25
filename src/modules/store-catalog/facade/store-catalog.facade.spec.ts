cecc91d4b4bbed24bd601403c254bc41cf69bf03
Daniel Major <daniel.alves@skytef.com.br>
2023-11-24 21:48:00 -0300

    Finalizando Store Catalog Facade

---
+++ src/modules/store-catalog/facade/store-catalog.facade.spec.ts
@@ -0,0 +1,40 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../repository/product.model";
+import StoreCatalogFacade from "./store-catalog.facade";
+import StoreCatalogFacadeFactory from "../factory/facade.factory";
+
+describe("StoreCatalogFacade test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should find a product", async () => {
+    const facade = StoreCatalogFacadeFactory.create();
+    await ProductModel.create({
+      id: "1",
+      name: "Product 1",
+      description: "Description 1",
+      salesPrice: 100,
+    });
+
+    const result = await facade.find({ id: "1" });
+    expect(result.id).toBe("1");
+    expect(result.name).toBe("Product 1");
+    expect(result.description).toBe("Description 1");
+    expect(result.salesPrice).toBe(100);
+  });
+});
